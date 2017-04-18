2293d16d493d41285d63984303152a6f35f01868
THUSHAN <thushankavishka@gmail.com>
2017-04-18 12:17:22 +0530

    Commenting on ISymbol.java interface

--- src/SlotMachine/ISymbol.java
+++ src/SlotMachine/ISymbol.java
@@ -13,7 +13,7 @@ import javax.swing.ImageIcon;
  * @author THUSHAN
  */
 public interface ISymbol {
-     //create all the public abstract methods.
+     //create all the public abstract methods..
     public void setImage(String imgPath);
 
     public String getImage();
